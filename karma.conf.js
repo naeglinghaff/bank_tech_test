fc4c8950c42c41964f67e159b93c6598a8a5a081
naeglinghaff <kate.o.haffenden@gmail.com>
2020-04-21 11:57:07 +0100

    Coverage report updated

--- karma.conf.js
+++ karma.conf.js
@@ -1,22 +1,24 @@
+
 module.exports = function(config) {
   config.set({
     basePath: '',
-    plugins: ['karma-jasmine', 'karma-coverage', 'karma-chrome-launcher'],
-    frameworks: ['jasmine'],
-    files: [
-      'src/*.js',
-      'spec/*.js'
-    ],
+    plugins: ['karma-browserify', 'karma-jasmine', 'karma-coverage', 'karma-chrome-launcher'],
+    frameworks: ['jasmine', 'browserify'],
+    files: ['./spec/*.js', './src/*.js'],
     browsers: ['Chrome'],
     singleRun: true,
     reporters: ['progress', 'coverage'],
-    preprocessors: { 'src/*.js': ['coverage'] },
+    preprocessors: { 'spec/*.js': ['browserify', 'coverage'], 'src/*.js': ['browserify', 'coverage'] },
+    browserify: {
+              watch: true,
+              debug: true
+          },
     coverageReporter: {
             includeAllSources: true,
             dir: 'coverage/',
             reporters: [
                 { type: 'text-summary' }
             ]
-        }
+        },
   });
 };
