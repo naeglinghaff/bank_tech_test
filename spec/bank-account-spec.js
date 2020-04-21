397c5c63a0d84fd67095321b5bff8ca601b8abff
naeglinghaff <kate.o.haffenden@gmail.com>
2020-04-21 14:51:06 +0100

    Updated test suite structured to follow BDD conventions

--- spec/bank-account-spec.js
+++ spec/bank-account-spec.js
@@ -10,28 +10,34 @@ describe('Bank Account', () => {
     jasmine.clock().mockDate(baseDate);
   })
 
-  it('should initialise with a balance of 0', () => {
-    expect(account._balance).toEqual(0);
+  describe('initilization', () => {
+    it('initialises with a balance of 0', () => {
+      expect(account._balance).toEqual(0);
+    })
   })
 
-  it('should create a new transaction when money is added or withdrawn', () => {
-    expect(account.addMoney(100)).toBeInstanceOf(Transaction);
-    expect(account.withdraw(100)).toBeInstanceOf(Transaction);
+  describe('adding money', () => {
+    it('creates a new transaction when money is added or withdrawn', () => {
+      expect(account.addMoney(100)).toBeInstanceOf(Transaction);
+      expect(account.withdraw(100)).toBeInstanceOf(Transaction);
+    })
+
+    it('updates the balance when money is added', () => {
+      account.addMoney(100);
+      expect(account._balance).toEqual(100.00);
+    })
   })
 
-  it('should update the balance when money is added', () => {
-    account.addMoney(100);
-    expect(account._balance).toEqual(100.00);
-  })
+  describe('withdrawing money', () => {
+    it('updates the balance when money is withdrawn', () => {
+      account.addMoney(100);
+      account.withdraw(50);
+      expect(account._balance).toEqual(50.00);
+    })
 
-  it('should update the balance when money is withdrawn', () => {
-    account.addMoney(100);
-    account.withdraw(50);
-    expect(account._balance).toEqual(50.00);
-  })
-
-  it('should raise error if there are insufficient funds', () => {
-    expect(function(){ account.withdraw(50); }).toThrow("Error: insufficient funds")
+    it('raises error if there are insufficient funds', () => {
+      expect(function(){ account.withdraw(50); }).toThrow("Error: insufficient funds")
+    })
   })
 
   afterEach( () => {
--- spec/transaction-spec.js
+++ spec/transaction-spec.js
@@ -6,21 +6,23 @@ describe('Transaction', () => {
     jasmine.clock().install();
   })
 
-  it('initializes with a timestamp', () => {
-    var baseDate = new Date(2020, 2, 12);
-    jasmine.clock().mockDate(baseDate);
-    transaction = new Transaction;
-    expect(transaction._created_at).toEqual(baseDate);
-  })
+  describe('initialization', () => {
+    it('initializes with a timestamp', () => {
+      var baseDate = new Date(2020, 2, 12);
+      jasmine.clock().mockDate(baseDate);
+      transaction = new Transaction;
+      expect(transaction._created_at).toEqual(baseDate);
+    })
 
-  it('initializes with a monetary value', () => {
-    transaction = new Transaction(25, 25);
-    expect(transaction._value).toEqual("25.00");
-  })
+    it('initializes with a monetary value', () => {
+      transaction = new Transaction(25, 25);
+      expect(transaction._value).toEqual("25.00");
+    })
 
-  it('initializes with the updated bank account balance', () => {
-    transaction = new Transaction(25, 25);
-    expect(transaction._current_balance).toEqual("25.00");
+    it('initializes with the updated bank account balance', () => {
+      transaction = new Transaction(25, 25);
+      expect(transaction._current_balance).toEqual("25.00");
+    })
   })
 
   afterEach( () => {
